c4960b07ef5b20130c08d2679704e53b0f31dddc
unknown <Nightmachine@gmail.com>
2018-11-28 16:37:45 -0700

    added conditional to render

--- src/index.js
+++ src/index.js
@@ -6,27 +6,34 @@ export default class App extends Component {
         super(props)
         this.state = {
             latitude: null,
-            longitute: null
+            errorMessage: ''
         }
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
+                    latitude: position.coords.latitude
                 })
             },
-            (err) => console.log(err)
+            (err) => {
+                this.setState({
+                    errorMessage: err.message
+                })
+            }
         )
     }
 
 
     render() {
-        return (
-            <div>
-                <div>Latitude: {this.state.latitude}</div>
-                <div>Longitude: {this.state.longitude}</div>
-            </div>
-        )
+        if (this.state.errorMessage && !this.state.latitude){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.latitude){
+            return <div>Latitude: {this.state.latitude}</div>;
+        }
+        
+        return <div>Loading...</div>;
+        
     }
 }
 
