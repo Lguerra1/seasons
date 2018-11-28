7edb7b1def26dbfbf13babf3764df68df635557a
unknown <Nightmachine@gmail.com>
2018-11-28 15:19:36 -0700

    added contructor and state

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class App extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
-            latitude: ""
+            latitude: null,
+            longitute: null
         }
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+            },
+            (err) => console.log(err)
+        )
     }
 
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        )
         return (
             <div>
-                Latitude:
+                <div>Latitude: {this.state.latitude}</div>
+                <div>Longitude: {this.state.longitude}</div>
             </div>
         )
     }
